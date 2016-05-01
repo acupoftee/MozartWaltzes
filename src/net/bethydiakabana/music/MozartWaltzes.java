199f8d0329f4cf1b9e941e9f5a5fd5a88e704f8e
BethyDiakabananas <bethyd123@gmail.com>
2016-05-01 19:01:35 -0400

    First commit

---
+++ src/net/bethydiakabana/music/MozartWaltzes.java
@@ -0,0 +1,112 @@
+package net.bethydiakabana.music;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/**
+ * MozartWaltzes.java simulates Wolfgang Amadeus Mozart's Musikalisches
+ * Würfelspiel. At every execution, a new pseudorandomly generated piece of
+ * music is composed and performed
+ * <p>
+ * In Mozart's game a two-part waltz (minuet and trio) is composed by joining
+ * together randomly selected, pre-composed measures.
+ * <p>
+ * The minuet is 16 measures long, having 176 possible pre-composed measures.
+ * The following table determines which pre-composed measures to play. Roll two
+ * dice for each of the 16 measures. The sum of the dice rolls is on the
+ * vertical axis; the measure of the minuet is on the horizontal axis.
+ * <p>
+ * The trio is also 16 measures long, with 96 possible pre-composed measures. To
+ * determine which one to play, roll one die.
+ * <p>
+ * Composing a minuet and trio in this manner allows for 11^16 * 6^16 = 1.3 *
+ * 10^29 different waltzes
+ * 
+ * @author Bethy Diakabana
+ *
+ */
+public class MozartWaltzes {
+	private final int[][] minuet = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 96, 22, 141, 41, 105, 122, 11, 30, 70, 121, 26, 9, 112, 49, 109, 14 },
+			{ 0, 32, 6, 128, 63, 146, 46, 134, 81, 117, 39, 126, 56, 174, 18, 116, 83 },
+			{ 0, 69, 95, 158, 13, 153, 55, 110, 24, 66, 139, 15, 132, 73, 58, 145, 79 },
+			{ 0, 40, 17, 113, 85, 161, 2, 159, 100, 90, 176, 7, 34, 67, 160, 52, 170 },
+			{ 0, 148, 74, 163, 45, 80, 97, 36, 107, 25, 143, 64, 125, 76, 136, 1, 93 },
+			{ 0, 104, 157, 27, 167, 154, 68, 118, 91, 138, 71, 150, 29, 101, 162, 23, 151 },
+			{ 0, 152, 60, 171, 53, 99, 133, 21, 127, 16, 155, 57, 175, 43, 168, 89, 172 },
+			{ 0, 119, 84, 114, 50, 140, 86, 169, 94, 120, 88, 48, 166, 51, 115, 72, 111 },
+			{ 0, 98, 142, 42, 156, 75, 129, 62, 123, 65, 77, 19, 82, 137, 38, 149, 8 },
+			{ 0, 3, 87, 165, 61, 135, 47, 147, 33, 102, 4, 31, 164, 144, 59, 173, 78 },
+			{ 0, 54, 130, 10, 103, 28, 37, 106, 5, 35, 20, 108, 92, 12, 124, 44, 131 } };
+
+	private final int[][] trio = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+			{ 0, 72, 6, 59, 25, 81, 41, 89, 13, 36, 5, 46, 79, 30, 95, 19, 66 },
+			{ 0, 56, 82, 42, 74, 14, 7, 26, 71, 76, 20, 64, 84, 8, 35, 47, 88 },
+			{ 0, 75, 39, 54, 1, 65, 43, 15, 80, 9, 34, 93, 48, 69, 58, 90, 21 },
+			{ 0, 40, 73, 16, 68, 29, 55, 2, 61, 22, 67, 49, 77, 57, 87, 33, 10 },
+			{ 0, 83, 3, 28, 53, 37, 17, 44, 70, 63, 85, 32, 96, 12, 23, 50, 91 },
+			{ 0, 18, 45, 62, 38, 4, 27, 52, 94, 11, 92, 24, 86, 51, 60, 78, 31 } };
+
+	private static final int DIE_FACES = 6;
+	private static final int MEASURES = 16;
+	private static final Random diceRoller = new Random();
+
+	/**
+	 * Composes a waltz with psudeorandomly selected pre-recorded measures
+	 * 
+	 * @return true if all measure files are played
+	 */
+	public boolean composeWaltz() {
+		List<String> waltzMeasures = selectMeasures();
+		List<File> audioFiles = new ArrayList<File>();
+		Clip clip;
+		boolean played = false;
+		for (String measures : waltzMeasures)
+			audioFiles.add(new File("src/resources/" + measures + ".wav"));
+		for (File audioFile : audioFiles) {
+			try {
+				clip = AudioSystem.getClip();
+				clip.open(AudioSystem.getAudioInputStream(audioFile));
+				clip.start();
+				long sleeptime = (clip.getMicrosecondLength() / 1000) - 50; 
+				Thread.sleep(sleeptime);
+			} catch (LineUnavailableException | IOException | UnsupportedAudioFileException | InterruptedException e) {
+				e.printStackTrace();
+				break;
+			} // end try-catch
+			played = true;
+		} // end for
+		return played;
+	} // end method composeWaltz
+	
+	private List<String> selectMeasures() {
+		List<String> waltzMeasures = new ArrayList<String>();
+		for (int measure = 1; measure <= MEASURES; measure++) {
+			int diceRoll = throwDice(2); 
+			waltzMeasures.add("M" + minuet[diceRoll][measure]);
+		} // end for
+		for (int measure = 1; measure <= MEASURES; measure++) {
+			int diceRoll = throwDice(1);
+			waltzMeasures.add("T" + trio[diceRoll][measure]);
+		} // end for
+		return waltzMeasures;
+	} // end method composeMinuet
+
+	private int throwDice(int numberOfDice) {
+		int rolls = 0;
+		if (numberOfDice <= 0)
+			return rolls;
+		for (int i = 0; i < numberOfDice; i++)
+			rolls += (diceRoller.nextInt(DIE_FACES) + 1);
+		return rolls;
+	} // end method rollDiceForTrio
+} // enc class MozartWaltzes
---
+++ src/net/bethydiakabana/music/test/MozartWaltzesTest.java
@@ -0,0 +1,193 @@
+package net.bethydiakabana.music.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.junit.Test;
+
+import net.bethydiakabana.music.MozartWaltzes;
+
+/**
+ * Test for {@link MozartWaltzes}
+ * 
+ * @author Bethy Diakabana
+ *
+ */
+public final class MozartWaltzesTest {
+	private static final int TEST_ITERATIONS = 200;
+
+	/**
+	 * Tests the results of the private method <tt>throwDice()</tt> by rolling
+	 * two fair dice with two-hundred iterations. Returns true each iteration if
+	 * the result is between 2 and 12. The total will be the sum of the two
+	 * dice.
+	 * <p>
+	 * <b>NOTE</b>: this private method is tested to obtain maximum code
+	 * coverage for this project
+	 * 
+	 * @throws IllegalAccessException
+	 *             if test does not have access to the private method
+	 * @throws IllegalArgumentException
+	 *             if illegal arguements are passed to the reflecting method
+	 * @throws InvocationTargetException
+	 *             wraps around exceptions thrown by reflecting method
+	 * @throws NoSuchMethodException
+	 *             if method can't be found
+	 * @throws SecurityException
+	 *             if there's a breach in method security
+	 */
+	@Test
+	public void testRollDiceForMinuet() throws IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException {
+		@SuppressWarnings("rawtypes")
+		Class[] args = {Integer.TYPE};
+		Method method = MozartWaltzes.class.getDeclaredMethod("throwDice", args);
+		method.setAccessible(true);
+		int smallestDieNumber = 2;
+		int largestDieNumber = 12;
+		int smallestInvalidDieNumber = 1;
+		int largestInvalidDieNumber = 13;
+		int numberOfDice = 2;
+		int diceRoll = 0;
+		for (int i = 0; i < TEST_ITERATIONS; i++) {
+			diceRoll = (int) method.invoke(new MozartWaltzes(), numberOfDice);
+			assertTrue(smallestDieNumber <= diceRoll && diceRoll <= largestDieNumber);
+			assertFalse(diceRoll <= smallestInvalidDieNumber && diceRoll >= largestInvalidDieNumber);
+		} // end for
+	} // end testRollDiceForMinuet
+
+	/**
+	 * Tests the results of the private method <tt>throwDice()</tt> by rolling
+	 * one fair die with two-hundred iterations. Returns true each iteration if
+	 * the result is between 1 and 6.
+	 * <p>
+	 * <b>NOTE</b>: this private method is tested to obtain maximum code
+	 * coverage for this project
+	 * 
+	 * @throws IllegalAccessException
+	 *             if test does not have access to the private method
+	 * @throws IllegalArgumentException
+	 *             if illegal arguements are passed to the reflecting method
+	 * @throws InvocationTargetException
+	 *             wraps around exceptions thrown by reflecting method
+	 * @throws NoSuchMethodException
+	 *             if method can't be found
+	 * @throws SecurityException
+	 *             if there's a breach in method security
+	 */
+	@Test
+	public void testThrowDieForTrio() throws IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException, SecurityException {
+		@SuppressWarnings("rawtypes")
+		Class[] args = new Class[1];
+		args[0] = Integer.TYPE;
+		Method method = MozartWaltzes.class.getDeclaredMethod("throwDice", args);
+		method.setAccessible(true);
+		int smallestDieNumber = 1;
+		int largestDieNumber = 6;
+		int smallestInvalidDieNumber = 0;
+		int largestInvalidDieNumber = 7;
+		int numberOfDice = 1;
+		int diceRoll = 0;
+		for (int i = 0; i < TEST_ITERATIONS; i++) {
+			diceRoll = (int) method.invoke(new MozartWaltzes(), numberOfDice);
+			assertTrue(smallestDieNumber <= diceRoll && diceRoll <= largestDieNumber);
+			assertFalse(diceRoll <= smallestInvalidDieNumber && diceRoll >= largestInvalidDieNumber);
+		} // end for
+	} // end testRollDiceForMinuet
+
+	/**
+	 * Tests the results of the private method <tt>throwDice()</tt> by passing
+	 * 0 to the method parameter. Returns true if the return value is less than or 
+	 * equal to 0, or if the dice roll is not equal to zero when a positive number
+	 * integer passed as a parameter
+	 * <p>
+	 * <b>NOTE</b>: this private method is tested to obtain maximum code
+	 * coverage for this project
+	 * 
+	 * @throws IllegalAccessException
+	 *             if test does not have access to the private method
+	 * @throws IllegalArgumentException
+	 *             if illegal arguements are passed to the reflecting method
+	 * @throws InvocationTargetException
+	 *             wraps around exceptions thrown by reflecting method
+	 * @throws NoSuchMethodException
+	 *             if method can't be found
+	 * @throws SecurityException
+	 *             if there's a breach in method security
+	 */
+	@Test
+	public void testThrowDieForZeroAndBelow() throws NoSuchMethodException, SecurityException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+		@SuppressWarnings("rawtypes")
+		Class[] args = new Class[1];
+		args[0] = Integer.TYPE;
+		Method method = MozartWaltzes.class.getDeclaredMethod("throwDice", args);
+		method.setAccessible(true);
+		int numberOfDice = 0;
+		int expectedRolls = 0;
+		int actualRolls = 0;
+
+		actualRolls = (int) method.invoke(new MozartWaltzes(), numberOfDice);
+		assertEquals(expectedRolls, actualRolls);
+
+		numberOfDice = -1; 
+		actualRolls = (int) method.invoke(new MozartWaltzes(), numberOfDice);
+		assertEquals(expectedRolls, actualRolls);
+		
+		numberOfDice = 2;
+		actualRolls = (int) method.invoke(new MozartWaltzes(), numberOfDice);
+		assertFalse(expectedRolls == actualRolls);
+	} // end testThrowDieWithZeroDice
+	
+	/**
+	 * Tests the private method <tt>selectMeasures()</tt> by checking for
+	 * invalid file names in the resulting <tt>List</tt> of psudeorandomly
+	 * selected minuet and trio measures. Returns true if the list does not
+	 * contain invalid measures
+	 * <p>
+	 * <b>NOTE</b>: this private method is tested to obtain maximum code
+	 * coverage for this project
+	 * 
+	 * @throws IllegalAccessException
+	 *             if test does not have access to the private method
+	 * @throws IllegalArgumentException
+	 *             if illegal arguements are passed to the reflecting method
+	 * @throws InvocationTargetException
+	 *             wraps around exceptions thrown by reflecting method
+	 * @throws NoSuchMethodException
+	 *             if method can't be found
+	 * @throws SecurityException
+	 *             if there's a breach in method security
+	 */
+	@Test
+	public void testSelectMeasures() throws NoSuchMethodException, SecurityException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException {
+		Method method = MozartWaltzes.class.getDeclaredMethod("selectMeasures");
+		method.setAccessible(true);
+		int measures = 32;
+		for (int i = 0; i < TEST_ITERATIONS; i++) {
+			@SuppressWarnings("unchecked")
+			List<String> waltzMeasures = (List<String>) method.invoke(new MozartWaltzes(), new Object[0]);
+			int arraySize = waltzMeasures.size();
+			assertTrue(arraySize == measures);
+			assertFalse(arraySize < measures || arraySize > measures);
+			assertFalse(waltzMeasures.contains("M0") || waltzMeasures.contains("T0"));
+			assertFalse(waltzMeasures.contains("M178") || waltzMeasures.contains("T97"));
+		} // end for
+	} // end method testComposeWaltzes
+
+	/**
+	 * Returns when the method is able to find every measure file and play it
+	 * without interruption
+	 */
+	@Test
+	public void testComposeWalts() {
+		assertTrue(new MozartWaltzes().composeWaltz());
+	} // end method testComposeWaltz
+}
